31e3cdf5923964071571082f2e3b63549982e2b8
Curtis Barnhart <cbarnhart@westmont.edu>
2025-04-15 15:05:02 -0700

    finished all

--- hello.py
+++ hello.py
@@ -1,36 +1,76 @@
 from flask import Flask, request
 from random import choice, randrange
 
-runways: list[int] = [ 42, 43, 62, 179, 180]
+runways: list[int] = [42, 43, 62, 179, 180]
+takeoffs: dict[int, int]
+landings: dict[int, int]
 secret_runway: int = -2
 app = Flask(__name__)
 
 @app.route("/departing/taxi", methods=['GET'])
 def departing_taxi():
     runway_choice: int
-    plane_id = request.args.get("plane_id")
     try:
-        plane_int: int = int(plane_id)
+        plane_id: int = int(request.args.get("plane_id"))
     except:
         return "Bad plane_id value"
     if randrange(0, 100) == 0:
         runway_choice = secret_runway
-    runway_choice = choice(runways)
+    elif plane_id < 2000:
+        runway_choice = choice(runways[:-2])
+    else:
+        runway_choice = choice(runways)
+    takeoffs[plane_id] = runway_choice
     return "Runway {:d}".format(runway_choice)
+
 
 @app.route("/departing/takeoff", methods=['GET'])
 def departing_takeoff():
-    plane_id = request.args.get("plane_id")
-    runway = request.args.get("runway")
-    return "Not Cleared"
+    try:
+        plane_id = int(request.args.get("plane_id"))
+    except:
+        return "Bad plane_id value"
+    try:
+        runway = int(request.args.get("runway"))
+    except:
+        return "Bad runway value"
+
+    if runway not in runways:
+        return "Nonexistent runway"
+    if takeoffs[plane_id] != runway:
+        return "Not cleared"
+    return "Cleared"
+
 
 @app.route("/arriving/where", methods=['GET'])
 def arriving_where():
-    plane_id = request.args.get("plane_id")
-    return "Runway 420"
+    runway_choice: int
+    try:
+        plane_id: int = int(request.args.get("plane_id"))
+    except:
+        return "Bad plane_id value"
+    if randrange(0, 100) == 0:
+        runway_choice = secret_runway
+    elif plane_id < 2000:
+        runway_choice = choice(runways[:-2])
+    else:
+        runway_choice = choice(runways)
+    landings[plane_id] = runway_choice
+    return "Runway {:d}".format(runway_choice)
 
 @app.route("/arriving/clearance", methods=['GET'])
 def arriving_clearance():
-    plane_id = request.args.get("plane_id")
-    runway = request.args.get("runway")
-    return "Not Cleared"
+    try:
+        plane_id = int(request.args.get("plane_id"))
+    except:
+        return "Bad plane_id value"
+    try:
+        runway = int(request.args.get("runway"))
+    except:
+        return "Bad runway value"
+
+    if runway not in runways:
+        return "Nonexistent runway"
+    if landings[plane_id] != runway:
+        return "Not cleared"
+    return "Cleared"
