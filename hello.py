56c7fee8366dcc7b92a05e8f7fe40949d9619111
Curtis Barnhart <cbarnhart@westmont.edu>
2025-04-15 14:53:42 -0700

    made secret runway

--- hello.py
+++ hello.py
@@ -1,11 +1,22 @@
 from flask import Flask, request
+from random import choice, randrange
 
+runways: list[int] = [ 42, 43, 62, 179, 180]
+secret_runway: int = -2
 app = Flask(__name__)
 
 @app.route("/departing/taxi", methods=['GET'])
 def departing_taxi():
+    runway_choice: int
     plane_id = request.args.get("plane_id")
-    return "Runway 69"
+    try:
+        plane_int: int = int(plane_id)
+    except:
+        return "Bad plane_id value"
+    if randrange(0, 100) == 0:
+        runway_choice = secret_runway
+    runway_choice = choice(runways)
+    return "Runway {:d}".format(runway_choice)
 
 @app.route("/departing/takeoff", methods=['GET'])
 def departing_takeoff():
