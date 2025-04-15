387876f781848144fa47c80a1ac917f7b01e68a6
Curtis Barnhart <cbarnhart@westmont.edu>
2025-04-15 14:49:53 -0700

    added more endpoints

--- hello.py
+++ hello.py
@@ -1,9 +1,25 @@
-# hello.py
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/api/myservice", methods=['GET'])
-def hello_world():
-    return "Hello, World!"
+@app.route("/departing/taxi", methods=['GET'])
+def departing_taxi():
+    plane_id = request.args.get("plane_id")
+    return "Runway 69"
+
+@app.route("/departing/takeoff", methods=['GET'])
+def departing_takeoff():
+    plane_id = request.args.get("plane_id")
+    runway = request.args.get("runway")
+    return "Not Cleared"
+
+@app.route("/arriving/where", methods=['GET'])
+def arriving_where():
+    plane_id = request.args.get("plane_id")
+    return "Runway 420"
+
+@app.route("/arriving/clearance", methods=['GET'])
+def arriving_clearance():
+    plane_id = request.args.get("plane_id")
+    runway = request.args.get("runway")
+    return "Not Cleared"
